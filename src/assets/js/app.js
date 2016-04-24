106fbe8271ffda835cb5abcba7f585fce2e5b707
Kobe Osei-Akoto <kobe.oseiakoto@gmail.com>
2016-04-23 20:43:30 -0400

    Adding topbar scroll event listener

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -12,3 +12,20 @@ $(".close").on("click", function() {
 $("#menu").on("click", "li", function() {
 	$("#menu").toggleClass("is-open");
 });
+
+
+var topbar, yPos;
+
+function yScroll() {
+	topbar = document.getElementsByClassName('top-bar')[0];
+	yPos = window.pageYOffset;
+
+	if (yPos > 10) {
+		topbar.style.backgroundColor = '#e6e6e6';
+	} else {
+		topbar.style.backgroundColor = '#FFFFFF';
+	}
+}
+
+
+window.addEventListener('scroll', yScroll);
